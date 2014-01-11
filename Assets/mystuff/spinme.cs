28a2b50b9a843d905ec05d1f6612662ae38c3e63
sharpieboss <jamesonrobe@gmail.com>
2014-01-11 13:44:23 -0500

    start
    first commit

---
+++ Assets/mystuff/spinme.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spinme : MonoBehaviour
+{
+    public float speed = 100f;
+    
+    
+    void Update ()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+}
